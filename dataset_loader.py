60170a3d452e1a64e5149b5165c3f4e8d2fb12b2
Marco Muscas <muscas.marco@gmail.com>
2019-12-09 22:51:56 +0100

    update and more comments

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,27 +1,26 @@
 import pandas as pd
 
+''' Just a class to load the datasets we are using, the point is having 
+    more readability. '''
+
 class DatasetLoader:
 
-    def __init__(self, name, dataset_folder=''):
+    def __init__(self, name, dataset_root_path='./dataset/'):
 
-        self.name = name
-
+        ''' Just because why not, could be useful if the variable name is not ideal when this class is istantiated. '''
+        self.name = name 
+        
         if self.name == 'coordinates':
-            self.dataset_path = dataset_folder+'USA-road-'+self.name+'.CAL.co'
+            self.dataset_path = dataset_root_path+'USA-road-'+self.name+'.CAL.co'
             self.cols = ['type','node-id', 'latitude','longitude']
 
         elif self.name == 'distance' or self.name == 'time-distance':
-            self.dataset_path = dataset_folder+'USA-road-'+self.name+'.CAL.gr'
-            self.cols = ['type','node-id-1', 'node-id-2', name]	
+            self.dataset_path = dataset_root_path+'USA-road-'+self.name+'.CAL.gr'
+            ''' Side note, distance and time-distance could have the same name for reusability'''
+            self.cols = ['type','node-id-1', 'node-id-2', 'distance']	
 
-
-        self.dataset = pd.read_csv(self.dataset_path, sep=' ', header=None)
-        self.dataset.columns = self.cols
+        self.dataset = pd.read_csv(self.dataset_path, sep=' ', header=None) # The first row is not a header.
+        self.dataset.columns = self.cols # Assigning name to the columns of the newly made dataset
 
     def get_dataset(self):
         return self.dataset
-
-
-if __name__ == '__main__':
-    dl = DatasetLoader('time-distance', './dataset/')
-    ds = dl.dataset
--- utils.py
+++ utils.py
@@ -6,13 +6,14 @@ Created on Mon Dec  9 16:57:02 2019
 @author: marco
 """
 import networkx as nx
-import time
+import time # When not testing, this 'import time' can be omitted.
 from dataset_loader import DatasetLoader
+
 def make_graph(vertices_dataset, edges_dataset): 
     graph = nx.Graph()    
     # Adding vertices
     graph.add_nodes_from(vertices_dataset['node-id'].values)
-    # Adding edges
+    # Adding edges (Side note, we are adding edges via tuples structured as (node1, node2, value))
     graph.add_edges_from([(x[0],x[1],{'distance':x[2]}) for x in edges_dataset[['node-id-1','node-id-2','distance']].values])
     
     return graph
@@ -20,14 +21,16 @@ def make_graph(vertices_dataset, edges_dataset):
 
 if __name__ == '__main__':
     
-    vertices_dataset_loader = DatasetLoader('coordinates','./dataset/')
-    edges_dataset_loader = DatasetLoader('distance','./dataset/')
+    vertices_dataset_loader = DatasetLoader('coordinates', dataset_root_path='./dataset/')
+    edges_dataset_loader = DatasetLoader('distance',dataset_root_path='./dataset/')
     
     vertices_dataset = vertices_dataset_loader.dataset
     edges_dataset = edges_dataset_loader.dataset
     
+    ''' Test for making the dataset '''
     start_t = time.time()
     g = make_graph(vertices_dataset, edges_dataset)    
     end_t = time.time() - start_t
     
     print('%f seconds for building the whole graph. ' % end_t)
+    ''' End test here '''
