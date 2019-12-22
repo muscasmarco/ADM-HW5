cb4cf6bd54cce4195a481c5251e4be6c3c51b1e0
Marco Muscas <muscas.marco@gmail.com>
2019-12-22 12:56:56 +0100

    Added network distance support

--- utils.py
+++ utils.py
@@ -5,32 +5,31 @@ Created on Mon Dec  9 16:57:02 2019
 
 @author: marco
 """
-import networkx as nx
 import time # When not testing, this 'import time' can be omitted.
 from dataset_loader import DatasetLoader
 import pickle
 import os 
 
-def make_graph(vertices_dataset, edges_dataset): 
-    graph = nx.Graph()    
-    # Adding vertices
-    graph.add_nodes_from(vertices_dataset['node-id'].values)
-    # Adding edges (Side note, we are adding edges via tuples structured as (node1, node2, value))
-    graph.add_edges_from([(x[0],x[1],{'distance':x[2]}) for x in edges_dataset[['node-id-1','node-id-2','distance']].values])
-    
-    return graph
 
-def make_adj_list(distance_metric='distance'): # Can also be 'time-distance'
+''' This function return the adjacency list (actually a dict) for a given metric.
+    Since making it is time consuming, it will first try to look for the file containing it.
+    if it doesn't find it, then it generates a new one. '''
+def make_adj_list(distance_metric='distance'):
     
-    if distance_metric not in ['distance','time-distance']:
-        print("Distance metric not valid, please choose 'distance' or 'time-distance'. ")
-        return -1
+    if distance_metric not in ['distance','time-distance', 'network-distance']:
+        
+        print("Distance metric not valid, please choose 'distance','time-distance' or 'network-distance'. ")
+        return None
     
     vertices_dataset_loader = DatasetLoader('coordinates')
-    edges_dataset_loader = DatasetLoader(distance_metric)
     
-    vertices_df = vertices_dataset_loader.dataset
-    edges_df = edges_dataset_loader.dataset
+    if distance_metric in ['distance', 'time-distance']:
+        edges_dataset_loader = DatasetLoader(distance_metric)
+    else:
+        edges_dataset_loader = DatasetLoader('distance')# The actual distance will be ignored in this case
+
+    vertices_df = vertices_dataset_loader.dataset #Load the vertices dataset
+    edges_df = edges_dataset_loader.dataset # Then the vertices dataset.
     
     
     file_path = str('./adj_list_%s.pkl' % distance_metric)
@@ -40,7 +39,8 @@ def make_adj_list(distance_metric='distance'): # Can also be 'time-distance'
         adj_list = pickle.load(open(file_path,'rb')) # The file exists, no need to build it from scratch.
         
     else:
-    
+        # Important! 
+        # Since there might be some disconnected vertices, we need the vertices dataset 
         adj_list = {i:{} for i in vertices_df.index}
         
         print_progress = True # Set this flag to True to print progress percentage
@@ -51,8 +51,10 @@ def make_adj_list(distance_metric='distance'): # Can also be 'time-distance'
             if index % 10000 == 0 and print_progress:
                 print('%4f' % (index/dataset_size * 100))
                 
-                
-            node1, node2, dist = edge['node-id-1'],edge['node-id-2'],edge['distance']
+            if distance_metric == 'network-distance':
+                node1, node2, dist = edge['node-id-1'],edge['node-id-2'], 1 # Where 1 represent 1 hop from point to point
+            else:
+                node1, node2, dist = edge['node-id-1'],edge['node-id-2'], edge['distance'] 
             
             if node1 in adj_list.keys():
                 adj_list[node1][node2] = dist
@@ -67,12 +69,11 @@ def make_adj_list(distance_metric='distance'): # Can also be 'time-distance'
    
 if __name__ == '__main__':
     
-    
     ''' Test for making the dataset '''
     print('Starting test...')
     start_t = time.time()
     #g = make_graph(vertices_dataset, edges_dataset)    
-    adj_list = make_adj_list()
+    adj_list = make_adj_list('network-distance')
     end_t = time.time() - start_t
     
     print('%f seconds for building the adj list. ' % end_t)
